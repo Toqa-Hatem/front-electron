b51a0c7f60ff6f826f66a6052f34fb6b8ce3c218
Toukaraafat <tokaraafat25@gmail.com>
2024-10-26 20:49:03 +0200

    commit

--- js/style.js
+++ js/style.js
@@ -80,3 +80,29 @@ function printContent() {
   printWindow.document.close();
   printWindow.print();
 }
+// modal code
+// Get modal elements
+const modal = document.getElementById("exampleModal");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const closeModalFooterBtn = document.getElementById("closeModalFooterBtn");
+
+// Open modal when button is clicked
+openModalBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+// Close modal when close buttons are clicked
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+closeModalFooterBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close modal when clicking outside the modal content
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
